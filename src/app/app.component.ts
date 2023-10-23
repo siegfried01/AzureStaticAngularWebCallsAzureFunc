59ba6c2cb338578d63e15c0cd96c77eac095746d
siegfried01 <sheintze@hotmail.com>
2023-10-23 11:58:47 -0700

    Fix syntax error in yml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}! How are you today? Built at Mon Oct 23 11:29:05 2023</div>`,
+  template: `<div>Hello {{value}}! How are you today? Built at Mon Oct 23 11:54:36 2023</div>`,
 })
 export class AppComponent {
-  value = 'World ('+ new Date() + ')';
+  value = 'World';
 }
