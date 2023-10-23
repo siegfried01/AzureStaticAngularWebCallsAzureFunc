0c173933fbdbe76f301fe1b8fd01c468944bfeaa
siegfried01 <sheintze@hotmail.com>
2023-10-23 11:32:23 -0700

    add built at time stamp and current date-time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}! How are you today?</div>`,
+  template: `<div>Hello {{value}}! How are you today? Built at Mon Oct 23 11:29:05 2023</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  value = 'World ('+ new Date() + ')';
 }
