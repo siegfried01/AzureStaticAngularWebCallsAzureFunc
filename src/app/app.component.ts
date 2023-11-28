f94ccb7ca88623145de30ad178e6c6a81aede4c0
siegfried01 <sheintze@hotmail.com>
2023-11-28 10:34:17 -0800

    Test new build after deleting the web site from azure. See if new deployement token works in new secret.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Mon Oct 23 16:38:20 2023</div>`,
+  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Tue Nov 28 10:30:35 2023</div>`,
 })
 export class AppComponent {
   value = 'World! ' + new Date();
