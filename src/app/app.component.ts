31be0c0a58a65addd4d5ffff2ac656afaba3960c
siegfried01 <sheintze@hotmail.com>
2023-12-16 14:38:21 -0800

    Try  using snippets from https://www.npmjs.com/package/libsodium-wrappers and https://docs.github.com/en/rest/guides/encrypting-secrets-for-the-rest-api?apiVersion=2022-11-28

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Sat Dec 16 10:09:02 2023</div>`,
+  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Sat Dec 16 14:35:11 2023</div>`,
 })
 export class AppComponent {
   value = 'World! ' + new Date();
