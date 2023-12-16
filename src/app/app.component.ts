7dd72ee248a3cc4351267b3245e96351c99628a2
siegfried01 <sheintze@hotmail.com>
2023-12-16 10:10:55 -0800

    Specify secret name value instead of 'secretName'. Update time stamp.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Thu Dec 14 20:06:19 2023</div>`,
+  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Sat Dec 16 10:09:02 2023</div>`,
 })
 export class AppComponent {
   value = 'World! ' + new Date();
