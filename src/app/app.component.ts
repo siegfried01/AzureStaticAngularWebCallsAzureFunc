4422d2f622ffe68347f3617ff02f8140bf2a13cb
siegfried01 <sheintze@hotmail.com>
2024-01-27 18:17:10 -0800

    Update time stamp and staticwebapp.config.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Sat Dec 16 17:45:20 2023</div>`,
+  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Sat Jan 27 18:13:26 2024</div>`,
 })
 export class AppComponent {
   value = 'World! ' + new Date();
