d66726cf0e037b97d448d056243a70fdd0d76992
siegfried01 <sheintze@hotmail.com>
2024-02-03 19:31:47 -0800

    Try a new Azure AD B2C registration and update time stamp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Sat Jan 27 20:44:43 2024</div>`,
+  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Sat Feb  3 19:28:45 2024</div>`,
 })
 export class AppComponent {
   value = 'World! ' + new Date();
