2dd5d80565f2306f39d4e32250e1f3390b9b9a4f
siegfried01 <sheintze@hotmail.com>
2024-02-03 19:50:22 -0800

    Update timestamp again! See if we can recover from bad redirect URI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Sat Feb  3 19:39:56 2024</div>`,
+  template: `<div>Hello {{value}}! Try out new token for AZURE_STATIC_WEB_APPS_API_TOKEN_POLITE_CLIFF_0F03D401E? Built at Sat Feb  3 19:49:26 2024</div>`,
 })
 export class AppComponent {
   value = 'World! ' + new Date();
